b675ee32c42c6702a2d992646813200e0e630271
Ubuntu <ubuntu@ip-172-31-38-41.ec2.internal>
2019-09-29 17:49:25 +0000

    init

---
+++ random-order-main-loop.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ Plugin Name: Randomize Main Loop
+ Plugin URI: https://generalchicken.net
+ Description: Sets the main loop to a random order 
+ Version: 1.0
+ Author: John Dee
+ Author URI: https://generalchicken.net
+ GitHub Plugin URI: https://github.com/JohnDeeBDD/WP-Random-Order-Main-Loop
+ */
+
+namespace GeneralChicken;
+
+function randomizeMainLoop( $query ) {
+	if ( ! is_admin() && $query->is_main_query() ) {
+		$query->set( 'orderby', 'rand' );
+	}
+}
+add_action( 'pre_get_posts', 'GeneralChicken\randomizeMainLoop' );
